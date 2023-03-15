d7135c1885b1f77462fb47a9bdb8588b17a623d7
unknown <mehdiraza1232016@gmail.com>
2023-03-15 19:08:28 +0500

    initial

---
+++ app/page.js
@@ -0,0 +1,20 @@
+"use client";
+
+
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+
+
+    <main className={styles.main}>
+
+
+    </main>
+
+
+  )
+}
---
+++ app/post/page.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+import styles from "./style.css"
+
+const Post = () => {
+    return (
+
+
+        <div className="main_screen_base">
+            asfnaskldn
+
+        </div>
+
+    )
+
+}
+
+export default Post
